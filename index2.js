66f5ec99ffacb94331e9400906dd934f082b7a33
Archik-K <ar.kryatova@yandex.ru>
2023-03-29 16:12:56 +0300

    first commit

--- index2.js
+++ index2.js
@@ -16,3 +16,21 @@ document.querySelector(".slider-prev").addEventListener("click", function () {
 	}
 	sliderLine.style.left = -offset + "px";
 });
+
+
+
+/* 
+//Второй вариант с переиспользованием функции
+let offset = 0; //смещение от левого края
+const sliderLine= document.querySelector(".slider-line");
+
+//Функция moveSlider принимает направление движения (1 - вправо, -1 - влево)
+const moveSlider = (direstion) =>{
+	const sliderWidth = 400;
+	offset = (offset + sliderWidth * direstion + 2000) % 2000; //Вычисление нового смещения с учетом границ
+sliderLine.style.left = -offset + "px";//Установка нового значения свойства left у блока .slider-line
+};
+
+document.querySelector(".slider-next").addEventListener("click", () => moveSlider(1));
+document.querySelector(".slider-prev").addEventListener("click", () => moveSlider(-1));
+ */
